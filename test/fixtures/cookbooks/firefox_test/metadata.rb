7f44abf2de8d9bc06ca87ea8d5645b28e61d05f0
Dennis Hoer <dennis.hoer@pearson.com>
2015-11-18 05:56:35 -0700

    mozilla_firefox

--- test/fixtures/cookbooks/firefox_test/metadata.rb
+++
@@ -1,9 +0,0 @@
-name 'firefox_test'
-maintainer 'Tim Smith'
-maintainer_email 'tsmith@llnw.com'
-license 'Apache 2.0'
-description 'Tests Firefox installs'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
-
-depends 'firefox'
