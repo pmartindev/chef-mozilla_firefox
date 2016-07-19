1189fb3a3f4506dcd0e7c6789057e5129d3570a1
Dennis Hoer <dennis.hoer@pearson.com>
2016-07-19 00:00:52 -0700

    Base packages on platform_family

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default['mozilla_firefox']['lang'] = 'en-US'
 default['mozilla_firefox']['32bit_only'] = false # DEPRECATED! use x86_only
 default['mozilla_firefox']['x86_only'] = node['mozilla_firefox']['32bit_only']
 default['mozilla_firefox']['use_package_manager'] = false
-default['mozilla_firefox']['packages'] = case node['platform']
+default['mozilla_firefox']['packages'] = case node['platform_family']
                                          when 'debian'
                                            %w(bzip2 libasound2 libgtk2.0-0 libdbus-glib-1-2 libxt6)
                                          else
