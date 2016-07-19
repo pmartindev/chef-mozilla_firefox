1a432a42b6a70157582efcd409707e6cd9aca94b
Dennis Hoer <dennis.hoer@pearson.com>
2016-07-19 00:02:17 -0700

    Add packages

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,5 +7,6 @@ default['mozilla_firefox']['packages'] = case node['platform_family']
                                          when 'debian'
                                            %w(bzip2 libasound2 libgtk2.0-0 libdbus-glib-1-2 libxt6)
                                          else
-                                           %w(bzip2)
+                                           %w(bzip2 autoconf-213 gtk3-devel GConf2-devel dbus-glib-devel
+                                              yasm-devel alsa-lib-devel pulseaudio-libs-devel)
                                          end
