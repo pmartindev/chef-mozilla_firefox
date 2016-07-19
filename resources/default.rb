c40b3c31f49e02552c6dbca1a068d386d7f31eba
Dennis Hoer <dennis.hoer@pearson.com>
2016-07-18 21:39:19 -0700

    Remove unused attributes

--- resources/default.rb
+++ resources/default.rb
@@ -3,14 +3,14 @@ default_action :install
 
 attribute(:version, kind_of: String, name_attribute: true)
 attribute(:checksum, kind_of: String)
-attribute(:uri, kind_of: String, default: 'https://download.mozilla.org')
 attribute(:lang, kind_of: String, default: lazy { node['mozilla_firefox']['lang'] })
 attribute(:x86_only, kind_of: [TrueClass, FalseClass], default: lazy { node['mozilla_firefox']['x86_only'] })
 attribute(:use_package_manager, kind_of: [TrueClass, FalseClass],
                                 default: lazy { node['mozilla_firefox']['use_package_manager'] })
 attribute(:path, kind_of: [String, NilClass])
 attribute(:link, kind_of: [String, Array, NilClass])
-attribute(:packages, kind_of: [Array, NilClass], default: lazy { node['mozilla_firefox']['packages'] })
+attribute(:packages, kind_of: Array, default: lazy { node['mozilla_firefox']['packages'] })
+
 attribute(:windows_ini_source, kind_of: String, default: 'windows.ini.erb')
 attribute(:windows_ini_content, kind_of: String, default: lazy { { InstallDirectoryPath: :path } })
 attribute(:windows_ini_cookbook, kind_of: String, default: 'mozilla_firefox')
