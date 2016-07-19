b6b2dd20439b66663d5df3a4eaa27322a88b0d54
Dennis Hoer <dennis.hoer@pearson.com>
2016-07-19 00:31:05 -0700

    Cleanup libs

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,10 @@ default['mozilla_firefox']['use_package_manager'] = false
 default['mozilla_firefox']['packages'] = case node['platform_family']
                                          when 'debian'
                                            %w(bzip2 libasound2 libgtk2.0-0 libdbus-glib-1-2 libxt6)
+                                         when 'fedora'
+                                           %w(bzip2 autoconf-213 gtk3-devel GConf2-devel dbus-glib-devel
+                                              yasm-devel alsa-lib-devel pulseaudio-libs-devel)
                                          else
                                            %w(bzip2 gtk3-devel GConf2-devel dbus-glib-devel
-                                              yasm-devel alsa-lib-devel pulseaudio-libs-devel)
+                                              alsa-lib-devel pulseaudio-libs-devel)
                                          end
