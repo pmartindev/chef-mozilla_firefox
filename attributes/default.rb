2ec9f10b58de7d02bb908aaa2362df7bc94c4fc2
Dennis Hoer <dennis.hoer@pearson.com>
2016-07-18 23:32:51 -0700

    Add bzip2

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,11 @@ default['mozilla_firefox']['lang'] = 'en-US'
 default['mozilla_firefox']['32bit_only'] = false # DEPRECATED! use x86_only
 default['mozilla_firefox']['x86_only'] = node['mozilla_firefox']['32bit_only']
 default['mozilla_firefox']['use_package_manager'] = false
-default['mozilla_firefox']['packages'] = %w(libasound2 libgtk2.0-0 libdbus-glib-1-2 libxt6)
+default['mozilla_firefox']['packages'] = case node['platform']
+                                         when 'debian'
+                                           %w(bzip2 libasound2 libgtk2.0-0 libdbus-glib-1-2 libxt6)
+                                         when 'rhel'
+                                           %w(bzip2)
+                                         end
+
+
