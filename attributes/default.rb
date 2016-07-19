aa4107ad8367402996186bc8aa052548207d98f2
Dennis Hoer <dennis.hoer@pearson.com>
2016-07-19 00:16:34 -0700

    Remove bootstrap

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,8 @@ default['mozilla_firefox']['x86_only'] = node['mozilla_firefox']['32bit_only']
 default['mozilla_firefox']['use_package_manager'] = false
 default['mozilla_firefox']['packages'] = case node['platform_family']
                                          when 'debian'
-                                           %w(bzip2 wget libasound2 libgtk2.0-0 libdbus-glib-1-2 libxt6)
+                                           %w(bzip2 libasound2 libgtk2.0-0 libdbus-glib-1-2 libxt6)
                                          else
-                                           %w(bzip2 wget autoconf-213 gtk3-devel GConf2-devel dbus-glib-devel
+                                           %w(bzip2 gtk3-devel GConf2-devel dbus-glib-devel
                                               yasm-devel alsa-lib-devel pulseaudio-libs-devel)
                                          end
