3ff597db4253081a56e7f8a43bc43582b39b471b
Dennis Hoer <dennis.hoer@pearson.com>
2016-09-21 22:21:24 -0700

    ESR now 45.4.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require_relative '../libraries/default'
 
-VER = '45.3.0esr'.freeze
+VER = '45.4.0esr'.freeze
 
 at_exit { ChefSpec::Coverage.report! }
