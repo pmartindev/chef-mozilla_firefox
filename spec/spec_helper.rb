3726f471e985622f309a8e4381503c238075a304
Dennis Hoer <dennis.hoer@pearson.com>
2017-06-12 13:23:02 -0700

    Update firefox version in unit test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require_relative '../libraries/default'
 
-VER = '52.0.1esr'.freeze
+VER = '52.1.2esr'.freeze
 
 at_exit { ChefSpec::Coverage.report! }
