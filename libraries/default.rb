44ce0599484ab009b484d4e35c24731e6ab2322b
Will Soula <william.soula@drillinginfo.com>
2018-08-01 12:39:34 -0600

    Only add user for linux OS

--- libraries/default.rb
+++ libraries/default.rb
@@ -31,10 +31,14 @@ module MozillaFirefox
     cmd = Mixlib::ShellOut.new(command)
     # Firefox executable can no longer be run by root
     # https://bugzilla.mozilla.org/show_bug.cgi?id=1323302
-    system("useradd firefox_install")
-    cmd.user="firefox_install"
-    cmd.run_command
-    system("userdel firefox_install")
+    if RUBY_PLATFORM =~ /linux/
+      system("useradd firefox_install")
+      cmd.user="firefox_install"
+      cmd.run_command
+      system("userdel firefox_install")
+    else
+      cmd.run_command
+    end
     cmd.stdout.strip
   end
 end
